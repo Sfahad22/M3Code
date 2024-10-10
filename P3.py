2b5c63bdd18b7224f406cde2e79d1ec8d991b78b
Fahad <Shaikfahd702@gmail.com>
2024-10-10 14:36:48 -0500

    Add files via upload

---
+++ P3.py
@@ -0,0 +1,7 @@
+#Shaik Fahad
+#Oct 8, 2024
+
+radius = float (input ("enter radius value "))
+area = round (3.4*radius*radius)
+print(f"Thanks for using this to calculate area of a circle and here is the answer : ", area)
+#This program calculate area of a circle.
---
+++ P4.py
@@ -0,0 +1,8 @@
+#Shaik Fahad
+#Oct 10, 2024
+
+miles = float(input("enter number of miles car has driven : "))
+gallons = float(input("enter how many gallons of gas is used : "))
+MPG = miles/gallons
+print(f"here is the total MPG of your car : {MPG}")
+#This program compute MPG for a car.
